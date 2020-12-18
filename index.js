4ded75033148b989e92169c34321cc4020d8517b
loveyousomuch <loveyousomuch554@gmail.com>
2020-12-18 14:58:13 +0300

    src

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-document.querySelectorAll(".community-points-summary button")[0].click()
+!function() {
+  function getRandomTime() {
+    return (parseInt(Math.random()*10)) * 1000
+  }
+  let randomTime = getRandomTime()
+
+  function getChannelP$ints() {
+    // Set new random time for next try
+    randomTime = getRandomTime()
+
+    const clickButton = document.querySelectorAll(".community-points-summary button")[1]
+    if (!clickButton) return false;
+
+    clickButton.click()
+  }
+
+  setInterval(getChannelP$ints, randomTime)
+}();
