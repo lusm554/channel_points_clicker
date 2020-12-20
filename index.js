e4243dff3e50be3139f6bf3961e2870c77f276f5
loveyousomuch <loveyousomuch554@gmail.com>
2020-12-20 18:23:20 +0300

    random time&log

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
   function getRandomTime() {
     return (parseInt(Math.random()*10)) * 1000
   }
+  const date = () => {
+   const now = new Date()
+   return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+  }
   let randomTime = getRandomTime()
+  let i = 0, toId;
 
-  function getChannelP$ints() {
-    // Set new random time for next try
-    randomTime = getRandomTime()
+  function setTo() {
+   if (toId) clearTimeout(toId);
 
-    const clickButton = document.querySelectorAll(".community-points-summary button")[1]
-    if (!clickButton) return false;
-
-    clickButton.click()
+   randomTime = getRandomTime()
+   toId = setTimeout(getChannelP$ints, randomTime)
   }
 
-  setInterval(getChannelP$ints, randomTime)
+  function getChannelP$ints() {
+   const clickButton = document.querySelectorAll(".community-points-summary button")[1]
+   if (!clickButton) return false;
+    
+   clickButton.click()
+   console.log(`Collected: ${++i} ${date()}`)
+   setTo() 
+  }
+
+  setTimeout(getChannelP$ints, randomTime)
 }();
